d6fd7c044b0e5d065f32382d154b73f6f35436ed
Hadr42 <notownpappa@gmail.com>
2021-06-23 16:20:19 -0700

    major nav pass

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,21 @@
 import Link from 'next/link';
 import { useRouter } from "next/router";
+import clsx from 'clsx';
+import styles from './Nav.module.scss'
 
 export default function Nav(){ 
     const router = useRouter();
+    const pathCheck = (path) => {
+        if(router.pathname == path){
+            return styles.active
+        }
+    }
         return (
-            <ul>
-            <li><Link href="/">TEST</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <style jsx> {`
-                .active{
-                    background-color:aqua;
-                }
-            `}</style>
-            </ul>
+            <nav className={styles.nav}>
+                <ul className={styles.navList}>
+                    <li className={clsx(pathCheck('/'), styles.navItem)}><Link  href="/"><a className={styles.navLink}>TEST</a></Link></li>
+                    <li className={clsx(pathCheck('/blog'), styles.navItem)}><Link href="/blog"><a className={styles.navLink}>Blog</a></Link></li>
+                </ul>
+            </nav>
         )
     }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 // next.config.js
+const path = require('path')
+
 module.exports = {
     images: {
       domains: ['images.ctfassets.net'],
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
     },
   }
