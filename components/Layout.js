5972782dea48c736ecc445150bf0e18eb33796e7
Hadr42 <notownpappa@gmail.com>
2021-07-22 21:13:44 -0700

    adding scroll to top

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Header from './Header/Header';
+import ScrollToTop from './ScrollToTop/ScrollToTop';
 import Script from 'next/script';
 
 export default function Layout({ children }) {
@@ -13,6 +14,7 @@ export default function Layout({ children }) {
         	<p>Copyright 2021 Is It Worse Than...</p>
 		</div>
       </footer>
+      <ScrollToTop />
 		<Script src="https://kit.fontawesome.com/e5a98a2ef3.js" crossOrigin="anonymous"></Script>
     </div>
   )
---
+++ components/ScrollToTop/ScrollToTop.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import styles from './ScrollToTop.module.scss';
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Top: 0 takes us all the way back to the top of the page
+  // Behavior: smooth keeps it smooth!
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
+  useEffect(() => {
+    // Button is displayed after scrolling for 50 pixels
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 50) {
+        setIsVisible(true);
+
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button className={styles.scrollBtn} onClick={scrollToTop}>
+          <h3>Go up!</h3>
+        </button>
+      )}
+    </>
+  );
+}
