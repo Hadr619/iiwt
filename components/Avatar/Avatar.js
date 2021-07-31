176fff5b80417cfd9b11fd9d6f05fe51cfdc48f3
Perry Reid <notownpappa@gmail.com>
2021-07-31 15:46:08 -0700

    remove individual avatars

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -2,13 +2,7 @@ import Logo from "../svg/Logo";
 import styles from './Avatar.module.scss';
 export default function Avatar({author}) {
     const displayAvatar = () => {
-        if(author){
-            if(author.toLowerCase() == 'hadr'){
-                return <img src="/avatars/perry.jpeg" className={styles.avatar}/>
-            } else{
-                return <div className={styles.avatarSvg}><Logo /></div>
-            }
-        }
+        return <div className={styles.avatarSvg}><Logo /></div>
     }
     return (
         <div className={styles.author}>{displayAvatar()} {author}</div>
