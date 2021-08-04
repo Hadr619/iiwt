e985405a941be3257a9c45b438bb395d312d3727
Hadr42 <notownpappa@gmail.com>
2021-08-04 08:47:47 -0700

    adding favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import ScrollToTop from './ScrollToTop/ScrollToTop';
@@ -7,6 +8,16 @@ export default function Layout({ children }) {
 
 	
   return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="theme-color" content="#ffffff" />
+     </Head>
     <div className="layout">
     <Header />
         { children }
@@ -14,5 +25,6 @@ export default function Layout({ children }) {
       <ScrollToTop smooth />
 		<Script src="https://kit.fontawesome.com/e5a98a2ef3.js" crossOrigin="anonymous"></Script>
     </div>
+    </>
   )
 }
