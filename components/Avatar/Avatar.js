809982254cfc36d09fbd66715fdf033d73570c75
Perry Reid <notownpappa@gmail.com>
2023-03-27 17:10:30 -0700

    update blog page for better grid layout, add new avatar image for avatar component

--- components/Avatar/Avatar.js
+++ components/Avatar/Avatar.js
@@ -1,9 +1,21 @@
 import Logo from "../svg/Logo";
+import Image from 'next/image';
 import styles from './Avatar.module.scss';
 import clsx from "clsx";
 export default function Avatar({author, className}) {
+    const cleanName = author.split(' ').join('').toLowerCase();
     const displayAvatar = () => {
-        return <div className={styles.avatarSvg}><Logo /></div>
+        console.log(cleanName == 'rcro' ? 'jpg' : 'jpeg')
+            return (
+            <div className={clsx(styles.avatar)}>
+                <Image 
+                src={`/avatar-${cleanName}.${cleanName == 'rcro' ? 'jpg' : 'jpeg'}`}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                />
+            </div>
+            )
     }
     return (
         <div className={clsx(styles.author, className)}>{displayAvatar()} {author}</div>
