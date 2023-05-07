99f8c2a0b3ee23032a0ce277d4a5f5cf01b7c01f
Perry Reid <notownpappa@gmail.com>
2023-05-07 10:35:21 -0700

    adding filter for new episode page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useFlip } from 'react-easy-flip'
 import Filter from "../../components/Filter/Filter";
 import { createClient} from 'contentful';
-import BlogCard from '../../components/Card/BlogCard';
+import BlogCard from '../../components/Card/BlogCard/BlogCard';
 import clsx from 'clsx';
 import styles from './blog.module.scss';
 
@@ -66,7 +66,6 @@ const displayPostCount = (blogType) => {
         <section className={styles.blogWrapper}>
         <h4 className="h4">Blog Posts</h4>
         <Filter 
-          contentTypes={posts}
           types={uniqueTypes} 
           activeType={activeType} 
           setActiveType={setActiveType} 
--- pages/episodes/index.js
+++ pages/episodes/index.js
@@ -1,8 +1,8 @@
 
 import { NextSeo } from 'next-seo';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from "next/link";
-import EpisodeCard from '../../components/Card/EpisodeCard';
+import Filter from "../../components/Filter/Filter";
 import Feed from 'rss-to-json';
 import styles from './episodes.module.scss';
 import clsx from 'clsx';
@@ -22,7 +22,39 @@ export async function getStaticProps() {
 
 export default function Episodes({ episodes }) {
   const [items, setItems] = useState(episodes.items);
-  
+  const [activeType, setActiveType] = useState("All");
+	const [filtered, setFiltered] = useState([]); 
+  const uniqueTypes = [
+    "Episodes",
+    "End of the Month",
+    "Movie Night"
+  ]
+  console.log(activeType)
+  useEffect(() => {
+    if(activeType === "All"){
+        setFiltered(episodes.items)
+        return
+    }
+let newFiltered = "";
+        if(activeType == "Episodes"){
+          newFiltered = episodes.items.filter((episode) =>
+              episode.title.toLowerCase().includes("ep")
+            )
+        }
+        else if (activeType == "End of the Month"){
+          newFiltered = episodes.items.filter(episode =>
+            episode.title.toLowerCase().includes("end of the month")
+            )
+        }
+        else if (activeType == "Movie Night"){
+          newFiltered = episodes.items.filter(episode =>
+            episode.title.toLowerCase().includes("movie night")
+            )
+        }
+    setFiltered(newFiltered);
+console.log(newFiltered)
+},[activeType])
+
     return (
       <div>
         <NextSeo 
@@ -32,8 +64,15 @@ export default function Episodes({ episodes }) {
         <section className={styles.epsiodesSection}>
           <div className={clsx(styles.episodePage, "inner")}>
             <h4 className="h4">Check out our Episodes</h4>
+            <Filter 
+              types={uniqueTypes} 
+              activeType={activeType} 
+              setActiveType={setActiveType} 
+              setFiltered={setFiltered}
+              className={styles.filter}
+            />
             <div className={styles.episodeContainer}>
-            {items.map((ep) => {
+            {filtered.map((ep) => {
               const newText = {__html: ep.description.split('--')[0]};
                        return(
                 <div key={ep.id} className={styles.item}>
